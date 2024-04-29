0c74db846f210dd87b5cbe6e851749c097c28d8f
Žofie Tůmová <zofietumova@gmail.com>
2024-04-29 18:05:48 +0200

    header styling

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,9 +4,9 @@ import Navbar from "./Navbar";
 const About = () => {
   return (
     <div>
+      <Navbar />
       <h1>Home</h1>
       dobrý den vítejte na mém webu je to tu moc fajn
-      <Navbar />
     </div>
   );
 };
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,14 +2,13 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="flex">
-      <Link to="/">Home</Link>
-
-      <Link to="/about">About</Link>
-
-      <Link to="/contact">Contact</Link>
-
-    </nav>
+    <div className="header-board">
+      <nav className="flex">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </div>
   );
 };
 
