0b2d37771c5de978e5fcb5625282ba094e4bfaa1
Žofie Tůmová <zofietumova@gmail.com>
2024-04-30 15:01:48 +0200

    init tailwind

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="header-board">
-      <nav className="flex">
+      <nav className="flex justify-around">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
         <Link to="/contact">Contact</Link>
